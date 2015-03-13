8e3e35ea3910bb46bd510893080ba1e39d94361f
John (Jack) Butts <butts.102@osu.edu>
2015-03-13 11:31:36 -0400

    Initial Camera Object

---
+++ Sprint 4/Sprint_4/Camera Classes/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Design;
+
+namespace Sprint4
+{
+    class CameraController
+    {
+        private KeyboardState keyboardState;
+        private Game1 game;
+
+        ICommands currentCommand;
+        Dictionary<Keys, ICommands> commandLibrary;
+
+        public CameraController(Game1 game)
+        {
+            this.game = game;
+
+        }
+
+        
+    }
+}
