537a12e3db095c002c016d5f6d82f2f74e210565
John (Jack) Butts <butts.102@osu.edu>
2015-03-13 11:52:29 -0400

    More small changes to Camera

--- Sprint 4/Sprint_4/Camera Classes/CameraController.cs
+++ Sprint 4/Sprint_4/Camera Classes/CameraController.cs
@@ -13,11 +13,17 @@ namespace Sprint4
     {
        
         private Game1 game;
+        private Vector2 location;
+        private float height;
+        private float width;
+
 
         public CameraController(Game1 game)
         {
             this.game = game;
-
+            this.location = Vector2.Zero;
+            this.height = 0;
+            this.width = 0;
         }
 
         
