431df0aafb0df61ce1a2ff2360592fbc8e9b902b
Tom Kiener <kiener.17@osu.edu>
2015-03-30 18:05:29 -0400

    Fix things

--- Sprint 4/Sprint_4/Game States/LivesScreenGameState.cs
+++ Sprint 4/Sprint_4/Game States/LivesScreenGameState.cs
@@ -34,14 +34,14 @@ namespace Sprint4
         public void Draw(SpriteBatch spriteBatch)
         {
             Game1.GetInstance().GraphicsDevice.Clear(Color.Black);
-            Game1.GetInstance().gameCamera.LookAt(Game1.GetInstance().gameCamera.Origin);
-            spriteBatch.DrawString(font, "Lives: " + Game1.GetInstance().lives, new Vector2(Game1.GetInstance().gameCamera.Origin.X - 40, Game1.GetInstance().gameCamera.Origin.Y + 110), Color.White);
+            Game1.GetInstance().gameCamera.LookAt(Game1.GetInstance().gameCamera.CenterScreen);
+            spriteBatch.DrawString(font, "Lives: " + Game1.GetInstance().lives, new Vector2(Game1.GetInstance().gameCamera.CenterScreen.X - 40, Game1.GetInstance().gameCamera.CenterScreen.Y + 110), Color.White);
             if (Game1.GetInstance().lives < 2)
             {
-                spriteBatch.DrawString(font2, "What's wrong?", new Vector2(Game1.GetInstance().gameCamera.Origin.X - 100, Game1.GetInstance().gameCamera.Origin.Y + 30), Color.White);
-                spriteBatch.DrawString(font2, "Too ", new Vector2(Game1.GetInstance().gameCamera.Origin.X + 10, Game1.GetInstance().gameCamera.Origin.Y + 200), Color.White);
-                spriteBatch.DrawString(font2, "hard ", new Vector2(Game1.GetInstance().gameCamera.Origin.X + 45, Game1.GetInstance().gameCamera.Origin.Y + 200), Color.Red);
-                spriteBatch.DrawString(font2, "for you?", new Vector2(Game1.GetInstance().gameCamera.Origin.X + 90, Game1.GetInstance().gameCamera.Origin.Y + 200), Color.White);
+                spriteBatch.DrawString(font2, "What's wrong?", new Vector2(Game1.GetInstance().gameCamera.CenterScreen.X - 100, Game1.GetInstance().gameCamera.CenterScreen.Y + 30), Color.White);
+                spriteBatch.DrawString(font2, "Too ", new Vector2(Game1.GetInstance().gameCamera.CenterScreen.X + 10, Game1.GetInstance().gameCamera.CenterScreen.Y + 200), Color.White);
+                spriteBatch.DrawString(font2, "hard ", new Vector2(Game1.GetInstance().gameCamera.CenterScreen.X + 45, Game1.GetInstance().gameCamera.CenterScreen.Y + 200), Color.Red);
+                spriteBatch.DrawString(font2, "for you?", new Vector2(Game1.GetInstance().gameCamera.CenterScreen.X + 90, Game1.GetInstance().gameCamera.CenterScreen.Y + 200), Color.White);
             }
         }
     }
