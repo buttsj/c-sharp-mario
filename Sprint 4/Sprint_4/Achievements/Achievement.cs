d952d5c8b883f5636e209b5f7578c276794d5aac
John (Jack) Butts <butts.102@osu.edu>
2015-04-13 12:15:57 -0400

    Added a couple Achievement classes and fixed a typo in Tom's Quality Review

---
+++ Sprint 4/Sprint_4/Achievements/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint4
+{
+    class Achievement
+    {
+        public bool isUnlocked { get; set; }
+        public Texture2D image { get; set; }
+    }
+}
---
+++ Sprint 4/Sprint_4/Achievements/AchievementsManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint4
+{
+    class AchievementsManager
+    {
+        private List<Achievement> achievements;
+        private List<int> achievementKeeper;
+
+        public void AchievementUnlocked(Achievement ach)
+        {
+            ach.isUnlocked = true;
+        }
+
+        public AchievementsManager()
+        {
+            achievementKeeper = new List<int>();
+            achievements = new List<Achievement>();
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementBeatLevel") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementCoins") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementEat") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementFall") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementFireball") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementKoopa") });
+            achievements.Add(new Achievement() { isUnlocked = false, image = Game1.gameContent.Load<Texture2D>("achievementLife") });
+        }
+
+    }
+}
