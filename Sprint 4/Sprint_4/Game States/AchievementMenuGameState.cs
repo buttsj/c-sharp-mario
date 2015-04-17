b2f89a6395364c3da95784e0d4366ce1c86f616c
John (Jack) Butts <butts.102@osu.edu>
2015-04-16 20:35:34 -0400

    Achievement hint pictures added

--- Sprint 4/Sprint_4/Game States/AchievementMenuGameState.cs
+++ Sprint 4/Sprint_4/Game States/AchievementMenuGameState.cs
@@ -12,6 +12,13 @@ namespace Sprint4
         Game1 game;
         public GUI menu;
         int inputBuffer = 0;
+        Texture2D achCoins;
+        Texture2D achFall;
+        Texture2D achFireball;
+        Texture2D achHitEnemy;
+        Texture2D achLevel;
+        Texture2D achLife;
+        Texture2D achMushroom;
 
         public AchievementMenuGameState()
         {
@@ -21,6 +28,13 @@ namespace Sprint4
             menu.currentCommand = menu.options[0].Key;
             game.keyboardController = new TitleKeyController(menu);
             game.gamepadController = new TitlePadController();
+            achCoins = Game1.gameContent.Load<Texture2D>("Achievements/achCoins");
+            achFall = Game1.gameContent.Load<Texture2D>("Achievements/achFall");
+            achFireball = Game1.gameContent.Load<Texture2D>("Achievements/achFireball");
+            achHitEnemy = Game1.gameContent.Load<Texture2D>("Achievements/achHitEnemy");
+            achLife = Game1.gameContent.Load<Texture2D>("Achievements/achLife");
+            achLevel = Game1.gameContent.Load<Texture2D>("Achievements/achLevel");
+            achMushroom = Game1.gameContent.Load<Texture2D>("Achievements/achMushroom");
         }
 
         public void Update(GameTime gameTime)
@@ -39,6 +53,13 @@ namespace Sprint4
         {
             game.level.Draw(spriteBatch);
             menu.Draw(spriteBatch);
+            spriteBatch.Draw(achCoins, new Rectangle(84, 250, 75, 51), Color.White);
+            spriteBatch.Draw(achFall, new Rectangle(160, 250, 75, 51), Color.White);
+            spriteBatch.Draw(achFireball, new Rectangle(236, 250, 75, 51), Color.White);
+            spriteBatch.Draw(achHitEnemy, new Rectangle(84, 302, 75, 51), Color.White);
+            spriteBatch.Draw(achLevel, new Rectangle(160, 302, 75, 51), Color.White);
+            spriteBatch.Draw(achLife, new Rectangle(236, 302, 75, 51), Color.White);
+            spriteBatch.Draw(achMushroom, new Rectangle(160, 354, 75, 51), Color.White);
         }
     }
 }
